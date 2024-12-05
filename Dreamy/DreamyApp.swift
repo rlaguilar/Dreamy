439b9c61bd4c1d0fc253b63eb75c1a104e5fbeb1
Reynaldo Aguilar <rlac1990@gmail.com>
2024-12-05 23:40:11 +1100

    Initial Commit

---
+++ Dreamy/DreamyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamyApp.swift
+//  Dreamy
+//
+//  Created by Reynaldo Aguilar on 5/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DreamyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
