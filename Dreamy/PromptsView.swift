8944e20288372ecc2cf1cd14908c24c7df5e7f79
Reynaldo Aguilar <rlac1990@gmail.com>
2024-12-09 22:53:29 +1100

    Polish scroll interactions

--- Dreamy/PromptsView.swift
+++ Dreamy/PromptsView.swift
@@ -6,24 +6,33 @@ struct PromptsView: View {
     private let itemsSpacing: CGFloat = 6
 
     var body: some View {
-        ZStack {
-            Color(white: 0.1).ignoresSafeArea(.all)
+        ScrollView(.vertical) {
+            VStack(spacing: 24) {
+                Text(promptStore.prompts[0].text)
+                    .foregroundStyle(Color.white.opacity(0.7))
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                    .padding()
 
-            ScrollView(.horizontal, showsIndicators: false) {
-                MosaicLayout(viewportWidth: UIScreen.main.bounds.width, spacing: itemsSpacing) {
-                    ForEach(promptStore.prompts.flatMap { $0.imageNames }, id: \.self) {
-                        Image($0)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                ScrollView(.horizontal, showsIndicators: false) {
+                    MosaicLayout(viewportWidth: UIScreen.main.bounds.width, spacing: itemsSpacing) {
+                        ForEach(promptStore.prompts.flatMap { $0.imageNames }, id: \.self) {
+                            Image($0)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                        }
                     }
                 }
+                .scrollTargetBehavior(.paging)
+                .scrollClipDisabled()
+                // FIXME: Make scrollTargetBehavior(.paging) work without having these `itemSpacing` all over the place.
+                .padding(.leading, 2 * itemsSpacing)
+                .padding(.trailing, itemsSpacing)
             }
-            .scrollTargetBehavior(.paging)
-            .scrollClipDisabled()
-            // FIXME: Make scrollTargetBehavior(.paging) work without having these `itemSpacing` all over the place.
-            .padding(.leading, 2 * itemsSpacing)
-            .padding(.trailing, itemsSpacing)
+        }
+        .background {
+            Color(white: 0.1).ignoresSafeArea(.all)
         }
     }
 }
